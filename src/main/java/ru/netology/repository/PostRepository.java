3e06200f1d4addcff74928769f2495bd5e211bc3
Andrey <143511905+Iziumich@users.noreply.github.com>
2024-12-02 12:17:20 +0300

    Update PostRepository.java

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,46 +1,26 @@
 package ru.netology.repository;
 
-import ru.netology.exception.NotFoundException;
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 
+@Repository
 public class PostRepository {
-  private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
-  private final AtomicLong idCounter = new AtomicLong(1);
-
-  public List<Post> all() {
-    return posts.values().stream().collect(Collectors.toList());
-  }
-
-  public Optional<Post> getById(long id) {
-    return Optional.ofNullable(posts.get(id));
-  }
-
-  public Post save(Post post) {
-    if (post.getId() == 0) {
-      // Create new post
-      long newId = idCounter.getAndIncrement();
-      post.setId(newId);
-      posts.put(newId, post);
-    } else {
-      // Update existing post
-      if (posts.containsKey(post.getId())) {
-        posts.put(post.getId(), post);
-      } else {
-        throw new NotFoundException("Post with id " + post.getId() + " not found");
-      }
+    public List<Post> all() {
+        return Collections.emptyList();
     }
-    return post;
-  }
 
-  public void removeById(long id) {
-    if (posts.remove(id) == null) {
-      throw new NotFoundException("Post with id " + id + " not found");
+    public Optional<Post> getById(long id) {
+        return Optional.empty();
     }
-  }
+
+    public Post save(Post post) {
+        return post;
+    }
+
+    public void removeById(long id) {
+    }
 }
